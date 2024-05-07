bedc1f9703462eaf19cb165aad3680882e2ea75b
LeslieSanz <114875034+LeslieSanz@users.noreply.github.com>
2024-05-07 10:14:49 -0500

    agregando clase Reserva y Clase (pero faltan sus atributos)

---
+++ src/main/java/modelo/ReservaDTO.java
@@ -0,0 +1,6 @@
+package modelo;
+
+public class ReservaDTO {
+    Alumno alumno; //El tipo de dato es un objeto de la clase Alumno para poder acceder a sus atributos (nombre,apellido,ect)
+    ClaseDTO clase; 
+}
