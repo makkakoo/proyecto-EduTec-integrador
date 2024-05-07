a03c58105a64f5b1aeb7988bbb6e86b58bb6ff20
Walter <Wfalcon2003@outlook.es>
2024-05-07 00:27:38 -0500

    Base de datos listo, crjo

---
+++ src/main/java/Conexion/Conexion.java
@@ -0,0 +1,20 @@
+package Conexion ;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    Connection conn;
+    
+    public Connection getConexion(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/bd_edutec", "root", "");
+            System.out.println("Conexión exitosa");
+        }catch(ClassNotFoundException | SQLException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        return conn;
+    }
+}
---
+++ src/main/java/Conexion/prueba.java
@@ -0,0 +1,15 @@
+
+package Conexion;
+
+
+public class prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Conexion con = new Conexion();
+        con.getConexion();
+    }
+    
+}
