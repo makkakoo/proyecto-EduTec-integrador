2cfd8d13b85aca1f23f7a7f0d7c694b8c3285893
LeslieSanz <114875034+LeslieSanz@users.noreply.github.com>
2024-05-07 19:24:10 -0500

    agregando atributos de la clase ClaseDTO

--- src/main/java/modelo/ClaseDTO.java
+++ src/main/java/modelo/ClaseDTO.java
@@ -1,5 +1,49 @@
 package modelo;
 
 public class ClaseDTO {
-    
+    private String estado;
+    private String fecha;
+    private String cod_aula;
+    private Tema tema;
+    private Horario horario;
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getCod_aula() {
+        return cod_aula;
+    }
+
+    public void setCod_aula(String cod_aula) {
+        this.cod_aula = cod_aula;
+    }
+
+    public Tema getTema() {
+        return tema;
+    }
+
+    public void setTema(Tema tema) {
+        this.tema = tema;
+    }
+
+    public Horario getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Horario horario) {
+        this.horario = horario;
+    }
 }
