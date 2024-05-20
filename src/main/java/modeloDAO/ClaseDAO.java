8a2aecfdc856ce1632dbbc04c5ba7a39947e980a
Adrian <143036879+makkakoo@users.noreply.github.com>
2024-05-19 22:36:29 -0500

    AGREGANDO FILTROS

--- src/main/java/modeloDAO/ClaseDAO.java
+++ src/main/java/modeloDAO/ClaseDAO.java
@@ -195,4 +195,116 @@ public class ClaseDAO implements ClaseInterface {
         } 
         return lista;
     }
+    @Override
+    public ArrayList<ClaseDTO> listarporFiltros(String aula_amnbiente, String fecha, int id_hora, int tema_id_tema, String persona_dni) {
+        try {
+
+            String sentencia = "";
+
+            String coma = "";
+            
+            if (fecha != null) {
+                if (coma.equals("")) {
+                    sentencia += " rc.fecha = '" + fecha + "' \n ";
+                    coma = "and ";
+                }
+            }
+            System.out.println("coma: " + coma);
+            if (id_hora >= 0) {
+                if (coma.equals("")) {
+                    sentencia += " rc.id_hora = " + id_hora + " \n ";
+                    coma = "and ";
+                } else {
+                    sentencia += coma + " rc.id_hora = " + id_hora + " \n ";
+                }
+            }
+
+            if (tema_id_tema >= 0) {
+                if (coma.equals("")) {
+                    sentencia += " rc.tema_id_tema = " + tema_id_tema + " \n ";
+                    coma = "and ";
+                } else {
+                    sentencia += coma + " rc.tema_id_tema = " + tema_id_tema + " \n ";
+                }
+
+            }
+            if (!"Seleccionar".equals(persona_dni)) {
+                if (coma.equals("")) {
+                    sentencia += " rc.persona_dni = '" + persona_dni + "' \n ";
+                    coma = "and ";
+                }else{
+                    sentencia += coma + " rc.persona_dni = '" + persona_dni + "' \n ";
+                }
+
+            }
+            if(coma.equals("")){
+                sentencia += "rc.estado = 'En curso'";
+            }else{
+                sentencia += coma + " rc.estado = 'En curso'";
+            }
+            
+            System.out.println("texto: " + sentencia);
+
+//            
+            String sql = "SELECT\n"
+                    + "    rc.id_registro_clase,\n"
+                    + "    rc.aula_ambiente,\n"
+                    + "    rc.id_hora,\n"
+                    + "    rc.tema_id_tema,\n"
+                    + "    rc.persona_dni,\n"
+                    + "    rc.estado,\n"
+                    + "    rc.fecha,\n"
+                    + "    a.aforo - COUNT(rr.id_reserva) AS cupos_disponibles\n"
+                    + "FROM\n"
+                    + "    registro_clases rc\n"
+                    + "    INNER JOIN aula a ON rc.aula_ambiente = a.ambiente\n"
+                    + "    LEFT JOIN registro_reserva rr ON rc.id_registro_clase = rr.id_registro_clase\n"
+                    + "WHERE \n"
+                    + sentencia + " \n"
+                    
+                    + "GROUP BY\n"
+                    + "    rc.id_registro_clase,\n"
+                    + "    rc.aula_ambiente,\n"
+                    + "    rc.id_hora,\n"
+                    + "    rc.tema_id_tema,\n"
+                    + "    rc.persona_dni,\n"
+                    + "    rc.estado,\n"
+                    + "    rc.fecha,\n"
+                    + "    a.aforo\n"
+                    + "HAVING\n"
+                    + "    cupos_disponibles > 0;";
+            conn = con.getConexion();
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                objCLase = new ClaseDTO();
+
+                // Obtener el objeto Especialidad
+                String ambiente = rs.getString("aula_ambiente");
+                System.out.println("ambiente = " + ambiente);
+                a = ad.listarUno(ambiente);
+                objCLase.setAula(a);
+                objCLase.setFecha(rs.getString("fecha"));
+
+                int hora = rs.getInt("id_hora");
+                h = hd.listarUno(hora + "");
+                objCLase.setHorario(h);
+
+                int tema = rs.getInt("tema_id_tema");
+                t = td.listarUno(tema + "");
+                objCLase.setTema(t);
+
+                String persona = rs.getString("persona_dni");
+                p = pd.listarUno(persona);
+                objCLase.setPersona(p);
+
+                lista.add(objCLase);
+            }
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(AulaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return lista;
+
+    }
 }
